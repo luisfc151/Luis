694c766fd1938b5f80d86ba18dda35e484fb52a2
luisfc151 <cuadroluisfelipe20@gmail.com>
2025-03-05 22:25:56 -0500

    Se agrega el proyacto

---
+++ luis 2/Tienda.java
@@ -0,0 +1,21 @@
+impor java. util.Scarnne;
+
+public class tienda.java
+
+public static void main (String)[]args)
+
+Scarnner Scarnne=new Scarnne(System.int);
+float descuento = 0.15;
+float preciodecamiseta = 25; 
+float preciodepantalon = 30;
+float decuentocamiseta = preciodecamiseta*(1-descuento);
+float descuentopantalon = preciodepantalon*(1-descuento);
+float preciototal = (decuentocamiseta+descuentopantalon);
+out (preciototal);
+int cantidadcamiseta = 2;
+int cantidadpantalon  = 2;
+out (descuentopantalon*cantidadpantalon+decuentocamiseta+descuento);
+out descuentoadicional = decuentocamiseta (1-0,06);
+   
+    }
+}  
