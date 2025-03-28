d136c0a2ffa2240e437456cd930733cebdb7e34e
luis <cuadroluisfelipe20@gmail.com>
2025-03-28 16:59:09 -0500

    se borran archivos

--- luis 2/Numerofactorial.java
+++ luis 2/Numerofactorial.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class Numerofactorial {
     public static void main(String[] args) {
         int number;
-        //se ingresa el numero.
-        try ( //Se crea el scanner.
+        
+        try ( .
                 Scanner numero = new Scanner(System.in)) {
-            //se ingresa el numero.
+          
             System.out.println("ingrese el numero:");
             number = numero.nextInt();
-            //cerramo el scanner.
+         
         }
         int factorial = 1;
         int i = 1;
 
-        //Se crea el while.
+        
         while ( i <= number) {
           factorial *= i;
           i++;
